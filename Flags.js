a50726030a833a069d43db578add10cfba3af63e
Jonathan Schmold <me@jonathanschmold.ca>
2018-02-04 12:23:39 -0700

    Added valueOf operatorss

--- Flags.js
+++ Flags.js
@@ -2,27 +2,38 @@
 let Flags = (function() {
     // Prevent scope interference. We want the original arguments.
     let args = arguments
-    return (function() {
+    // Allow supports of valueOf
+    let flag = (function(name, argval, obj) {
+        obj[name] = this
+        this.Enable = () => obj.Value = obj.Value | argval
+        this.Disable = () => obj.Value = obj.Value & ~argval
+        this.IsEnabled = () => (obj.Value & argval) == argval
+        this.Value = argval   
+    })
+    // Make it easier to use
+    flag.prototype.valueOf = function() {
+        return this.IsEnabled()
+    }
+    // The generated class
+    let created = (function() {
         // We want the index of the key
+        this.Value = 0
         for(var key in args) {
             // Get the argument value to prevent pointing to only 1 var
             let arg = args[key]
-            // Create the flag itself
-            this.Value = 0
-            // Do a bitwise op to generate the flag
+            // Do a bitwise op to generate the flag's value
             let argval = 1 << key
             // Create an object that allows you to more easily check your flags object
-            this[arg] = {
-                "Value": argval,
-                "Enable": () => this.Value = this.Value | argval,
-                "Disable": () => this.Value = this.Value & ~argval,
-                "IsEnabled": () => (this.Value & argval) == argval
-            }
+            this[arg] = new flag(arg, argval, this)
         }
     })
+    created.prototype.valueOf = function() {
+        return this.Value;
+    }
+    return created;
 })
 
-
+// nodejs support
 if(module && module.exports) {
     module.exports = Flags
 }
